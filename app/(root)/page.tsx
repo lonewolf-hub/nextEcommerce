7e99457947191f2d23bc8a64e2f50eaa788f489d
Sidharth Shukla <swayam1044@gmail.com>
2023-07-20 10:45:47 +0530

    app inilization tested

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+
+export default function Home() {
+  return (
+   <div>
+    <h2>hello world</h2>
+    <Button>Click me</Button>
+   </div>
+  )
+}
